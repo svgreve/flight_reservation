8d7345bf5b0bb3aa30ca9f15c1b3e312d3d02d33
Sergio Vieira Greve <sgreve@eikon.com.br>
2021-03-19 13:47:35 -0300

    FlightReservationTest completed

--- test/flight_reservation_test.exs
+++ test/flight_reservation_test.exs
@@ -22,8 +22,30 @@ defmodule FlightReservationTest do
         name: "Sergio"
       }
 
-
       assert user == expected_user
+    end
+
+    test "when cpf is invalid, returns an error" do
+      FlightReservation.start_agents()
+
+      user_params = %{
+        name: "Sergio",
+        email: "sergio@banana.com",
+        cpf: "123456789"
+      }
+
+      response = FlightReservation.create_or_update_user(user_params)
+
+      expected_response = {:error, "Invalid CPF: 123456789"}
+      assert response == expected_response
+    end
+
+    test "when there are no user parameters, returns an error" do
+      FlightReservation.start_agents()
+      response = FlightReservation.create_or_update_user(%{})
+
+      expected_response = {:error, "Invalid parameters"}
+      assert response == expected_response
     end
   end
 end
